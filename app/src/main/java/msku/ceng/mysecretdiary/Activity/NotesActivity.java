e448a3552eee29f5b7632e10fc9c0b7690b60c9c
U190709050 <elf.prlk64@gmail.com>
2022-12-31 16:41:21 +0300

    profile done

--- app/src/main/java/msku/ceng/mysecretdiary/Activity/NotesActivity.java
+++ app/src/main/java/msku/ceng/mysecretdiary/Activity/NotesActivity.java
@@ -1,28 +1,34 @@
 package msku.ceng.mysecretdiary.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import java.util.List;
 
 import msku.ceng.mysecretdiary.Adapter.NotesAdapter;
+import msku.ceng.mysecretdiary.Fragments.ProfileFragment;
 import msku.ceng.mysecretdiary.Model.Notes;
 import msku.ceng.mysecretdiary.R;
 import msku.ceng.mysecretdiary.ViewModel.NotesViewModel;
 
-public class NotesActivity extends AppCompatActivity {
+public class NotesActivity extends AppCompatActivity{
 
     private Button saveButton; //add note button
     TextView noFilter, happyFilter, sadFilter, angryFilter, excitedFilter;
@@ -30,6 +36,10 @@ public class NotesActivity extends AppCompatActivity {
     RecyclerView recyclerView; //the view list item will show uğ
     NotesAdapter notesAdapter; //The Adapter which connects UI the live list
     String selected = "noFilter";
+    ImageView profileButton;
+    ImageView settingsButton;
+
+
 
 
     @Override
@@ -37,9 +47,6 @@ public class NotesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
 
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().hide();
-        }
         //conncecting variables to UI elements by ID
         recyclerView = findViewById(R.id.notesRecycler);
         notesViewModel = ViewModelProviders.of(this).get(NotesViewModel.class);
@@ -49,11 +56,19 @@ public class NotesActivity extends AppCompatActivity {
         sadFilter = findViewById(R.id.sadFilter);
         angryFilter = findViewById(R.id.angryFilter);
         excitedFilter = findViewById(R.id.excitedFilter);
+        profileButton = findViewById(R.id.profileButton);
+        settingsButton = findViewById(R.id.settingsButton);
 
 
-        switch(selected){
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(NotesActivity.this,ProfileActivity.class);
+                startActivity(i);
+            }
+        });
 
-        }
+
 
         noFilter.setBackgroundResource(R.drawable.filter_selected_shape);//select the no filter first
         noFilter.setOnClickListener(v -> {
@@ -109,6 +124,8 @@ public class NotesActivity extends AppCompatActivity {
        });
 
 
+
+
        notesViewModel.getAllNotes.observe(this, new Observer<List<Notes>>() {
            @Override
            public void onChanged(List<Notes> notes) {
@@ -116,6 +133,9 @@ public class NotesActivity extends AppCompatActivity {
            }
        });
     }
+
+
+
 
     public void setAdapter(List<Notes> notes){
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -159,5 +179,8 @@ public class NotesActivity extends AppCompatActivity {
                 }
             });
         }
+
+
     }
+
 }
